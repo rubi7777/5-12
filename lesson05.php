0f0e43f5d4a9bd5b987354d380de5052716f564d
るび7777 <rubibobu@gmail.com>
2025-08-06 09:38:55 +0900

    最新の変更を上書き

--- lesson05.php
+++ lesson05.php
@@ -1,1 +1,57 @@
 ﻿<?php
+
+// 1. Mediaインターフェース
+interface Media {
+    public function play();
+}
+
+// 2. Musicクラス
+class Music implements Media {
+    private $songTitle;
+
+    public function __construct($songTitle) {
+        $this->songTitle = $songTitle;
+    }
+
+    public function play() {
+        echo "{$this->songTitle} を再生中\n";
+    }
+}
+
+// 3. Videoクラス
+class Video implements Media {
+    private $videoTitle;
+
+    public function __construct($videoTitle) {
+        $this->videoTitle = $videoTitle;
+    }
+
+    public function play() {
+        echo "{$this->videoTitle} を再生中\n";
+    }
+}
+
+// 4. Podcastクラス
+class Podcast implements Media {
+    private $episodeTitle;
+
+    public function __construct($episodeTitle) {
+        $this->episodeTitle = $episodeTitle;
+    }
+
+    public function play() {
+        echo "{$this->episodeTitle} を再生中\n";
+    }
+}
+
+// === 動作確認 ===
+$mediaList = [
+    new Music("ありがとう"),
+    new Video("PHP入門講座"),
+    new Podcast("明日のニュース")
+];
+
+foreach ($mediaList as $media) {
+    $media->play();
+}
+
